8c09ca6ac84d48da78a64571c7ab08415adff5fb
Mani Mohan <manee@asu.edu>
2018-04-25 16:01:56 -0700

    addition of base files

---
+++ transfers.py
@@ -0,0 +1,20 @@
+f = open('schedule.txt', 'r')
+data = f.readlines()
+f.close()
+
+nteams = 8
+teams = ['MI', 'CSK', 'DD', 'KXIP', 'KKR', 'RCB', 'RR', 'DD']
+ngames = 56
+ntransfers = 80
+nplayers = 11
+
+MI = 0
+CSK = 0
+DD = 0
+KXIP = 0
+KKR = 0
+RCB = 0
+RR = 0
+DD = 0
+
+#goal is to keep schedule team players n+n = maximum of 11.  
